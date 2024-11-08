4205c624b2706211fe4f4c9f463a3bee6132d2c1
Brayan Alejandro Pedraza Leon <brayan_pedraza@javeriana.edu.co>
2024-11-08 11:06:35 -0500

    sin carpeta backend

--- pruebaPanzofi/settings.py
+++ pruebaPanzofi/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'backend')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
