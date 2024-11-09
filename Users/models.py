9cc20de01be57dabe1498fcdcc5936b0e6a7a164
Brayan Alejandro Pedraza Leon <brayan_pedraza@javeriana.edu.co>
2024-11-08 22:36:14 -0500

    arreglo zona horaria tz con localtime

--- Users/models.py
+++ Users/models.py
@@ -12,13 +12,13 @@ class Usuarios(models.Model):
 class Usuarios_Sesion(models.Model):
     ID_Usuarios_Sesion = models.AutoField(primary_key=True)
     ID_Usuarios = models.ForeignKey(Usuarios, on_delete=models.PROTECT, null=True)
-    Inicio_Sesion = models.DateTimeField(default=timezone.now)
+    Inicio_Sesion = models.DateTimeField(default=timezone.localtime(timezone.now()))
     Boton_1 = models.IntegerField(default=0)
     Boton_2 = models.IntegerField(default=0)
     Tiempo = models.IntegerField(default=0)
 
     def start_session(self):
-        self.Inicio_Sesion = timezone.now()
+        self.Inicio_Sesion = timezone.localtime(timezone.now())
         self.Boton_1 = 0  # Reiniciar contador de clics al iniciar sesión
         self.Boton_2 = 0  # Reiniciar contador de clics al iniciar sesión
         self.Tiempo = 0
@@ -32,7 +32,7 @@ class Usuarios_Sesion(models.Model):
             Auditoria_Usuarios_Sesion.objects.create(
                 ID_Usuarios=self.ID_Usuarios,
                 Inicio_Sesion=self.Inicio_Sesion,
-                Cierre_Sesion=timezone.now(),
+                Cierre_Sesion=timezone.localtime(timezone.now()),
                 Tiempo=self.Tiempo,
                 Boton_1=self.Boton_1,
                 Boton_2=self.Boton_2
